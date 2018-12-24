a3da27d5a191b534499cecc3cfa5c09b98f2d467
Ivan St <brokeyourbike@gmail.com>
2018-12-24 11:44:19 +0200

    hit and category

---
+++ admin/model/extension/module/pro_znachek.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ *  location: admin/model
+ */
+class ModelExtensionModuleProZnachek extends Model
+{
+    public function getZnachki()
+    {
+        return array(
+            'new'   => 'Значек "Новинка"',
+            'act'   => 'Значек "Акция"',
+            'hit'   => 'Значек "Хит"',
+        );
+    }
+}
