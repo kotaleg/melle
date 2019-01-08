b245f4c7124ed8587f5d688b3c5d45cf7b04dd14
brokeyourbike <brokeyourbike@gmail.com>
2019-01-08 11:42:48 +0300

    ~

--- admin/config.php
+++ admin/config.php
@@ -8,30 +8,30 @@ define('HTTPS_SERVER', 'http://localhost/302/melle/admin/');
 define('HTTPS_CATALOG', 'http://localhost/302/melle/');
 
 // DIR
-define('DIR_APPLICATION', 'C:/XAMP71/htdocs/302/melle/admin/');
-define('DIR_SYSTEM', 'C:/XAMP71/htdocs/302/melle/system/');
-define('DIR_DATABASE', 'C:/XAMP71/htdocs/302/melle/system/database/');
-define('DIR_LANGUAGE', 'C:/XAMP71/htdocs/302/melle/admin/language/');
-define('DIR_TEMPLATE', 'C:/XAMP71/htdocs/302/melle/admin/view/template/');
-define('DIR_CONFIG', 'C:/XAMP71/htdocs/302/melle/system/config/');
-define('DIR_IMAGE', 'C:/XAMP71/htdocs/302/melle/image/');
-define('DIR_DOWNLOAD', 'C:/XAMP71/htdocs/302/melle/system/storage/download/');
-define('DIR_CACHE', 'C:/XAMP71/htdocs/302/melle/system/storage/cache/');
-define('DIR_LOGS', 'C:/XAMP71/htdocs/302/melle/system/storage/logs/');
+define('DIR_APPLICATION', 'C:/xampp/htdocs/302/melle/admin/');
+define('DIR_SYSTEM', 'C:/xampp/htdocs/302/melle/system/');
+define('DIR_DATABASE', 'C:/xampp/htdocs/302/melle/system/database/');
+define('DIR_LANGUAGE', 'C:/xampp/htdocs/302/melle/admin/language/');
+define('DIR_TEMPLATE', 'C:/xampp/htdocs/302/melle/admin/view/template/');
+define('DIR_CONFIG', 'C:/xampp/htdocs/302/melle/system/config/');
+define('DIR_IMAGE', 'C:/xampp/htdocs/302/melle/image/');
+define('DIR_DOWNLOAD', 'C:/xampp/htdocs/302/melle/system/storage/download/');
+define('DIR_CACHE', 'C:/xampp/htdocs/302/melle/system/storage/cache/');
+define('DIR_LOGS', 'C:/xampp/htdocs/302/melle/system/storage/logs/');
 
-define('DIR_MODIFICATION', 'C:/XAMP71/htdocs/302/melle/system/storage/modification/');
-define('DIR_UPLOAD', 'C:/XAMP71/htdocs/302/melle/system/storage/upload/');
+define('DIR_MODIFICATION', 'C:/xampp/htdocs/302/melle/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/xampp/htdocs/302/melle/system/storage/upload/');
 define('DIR_STORAGE', DIR_SYSTEM . 'storage/');
-define('DIR_SESSION', 'C:/XAMP71/htdocs/302/melle/system/storage/session/');
+define('DIR_SESSION', 'C:/xampp/htdocs/302/melle/system/storage/session/');
 
-define('DIR_CATALOG', 'C:/XAMP71/htdocs/302/melle/catalog/');
+define('DIR_CATALOG', 'C:/xampp/htdocs/302/melle/catalog/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
 define('DB_HOSTNAME', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', 'root');
-define('DB_DATABASE', 'opencart__302_10');
+define('DB_DATABASE', 'melle.new');
 define('DB_PORT', '3306');
 define('DB_PREFIX', 'oc_');
 // OpenCart API
