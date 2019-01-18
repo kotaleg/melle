05f0fd03823856fbcef26aef1f016f0e1153fac5
Ivan St <brokeyourbike@gmail.com>
2019-01-18 14:22:15 +0200

    fix old styles

--- webpack.mix.js
+++ webpack.mix.js
@@ -5,13 +5,17 @@ const CODENAME = 'melle'
 const CATALOG_JS_PATH = path.join('catalog/view/javascript', CODENAME)
 const CATALOG_CSS_PATH = path.join('catalog/view/theme/', CODENAME, 'stylesheet/')
 
-// mix.js(path.join(CATALOG_JS_PATH, 'src/main.js'), path.join(CATALOG_JS_PATH, 'dist/'+CODENAME+'.js'))
+mix.js(path.join(CATALOG_JS_PATH, 'src/main.js'), path.join(CATALOG_JS_PATH, 'dist/'+CODENAME+'.js'))
 mix.sass(path.join(CATALOG_CSS_PATH, 'main.scss'), CODENAME+'.css');
 
-mix.setPublicPath(path.join(CATALOG_CSS_PATH, 'dist/'))
+mix.setPublicPath(path.join(CATALOG_JS_PATH, 'dist/'))
 
 mix.options({
     // extractVueStyles: true,
     // purifyCss: true,
     // postCss: [require('autoprefixer')]
 })
+
+if (mix.inProduction()) {
+    mix.version();
+}
