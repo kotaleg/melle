e2697b419f23574e466c46a68679c6909ed9494a
Ivan St <brokeyourbike@gmail.com>
2019-02-07 16:34:19 +0000

    ~

--- catalog/controller/startup/sass.php
+++ catalog/controller/startup/sass.php
@@ -3,13 +3,13 @@ class ControllerStartupSass extends Controller {
     public function index() {
         $file = DIR_APPLICATION . 'view/theme/' . $this->config->get('theme_' . $this->config->get('config_theme') . '_directory') . '/stylesheet/bootstrap.css';
 
-        if (!is_file($file) || (is_file(DIR_APPLICATION . 'view/theme/' . $this->config->get('theme_' . $this->config->get('config_theme') . '_directory') . '/stylesheet/sass/bootstrap.scss') && !$this->config->get('developer_sass'))) {
+        if (!is_file($file) || (is_file(DIR_APPLICATION . 'view/theme/' . $this->config->get('theme_' . $this->config->get('config_theme') . '_directory') . '/stylesheet/sass/_bootstrap.scss') && !$this->config->get('developer_sass'))) {
             include_once(DIR_STORAGE . 'vendor/scss.inc.php');
 
             $scss = new Scssc();
             $scss->setImportPaths(DIR_APPLICATION . 'view/theme/' . $this->config->get('theme_' . $this->config->get('config_theme') . '_directory') . '/stylesheet/sass/');
 
-            $output = $scss->compile('@import "bootstrap.scss"');
+            $output = $scss->compile('@import "_bootstrap.scss"');
 
             $handle = fopen($file, 'w');
 
