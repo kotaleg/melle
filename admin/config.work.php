fbf3238c3bf47804e235f087acf9439f9c251e71
Ivan St <brokeyourbike@gmail.com>
2019-02-18 04:52:35 +0200

    ~

--- admin/config.work.php
+++ admin/config.work.php
@@ -1,30 +1,30 @@
 <?php
 // HTTP
-define('HTTP_SERVER', 'http://test.melle.online/admin/');
-define('HTTP_CATALOG', 'http://test.melle.online/');
+define('HTTP_SERVER', 'http://work.melle.online/admin/');
+define('HTTP_CATALOG', 'http://work.melle.online/');
 
 // HTTPS
-define('HTTPS_SERVER', 'http://test.melle.online/admin/');
-define('HTTPS_CATALOG', 'http://test.melle.online/');
+define('HTTPS_SERVER', 'http://work.melle.online/admin/');
+define('HTTPS_CATALOG', 'http://work.melle.online/');
 
 // DIR
-define('DIR_APPLICATION', '/home/web/test.melle.online/www/admin/');
-define('DIR_SYSTEM', '/home/web/test.melle.online/www/system/');
-define('DIR_DATABASE', '/home/web/test.melle.online/www/system/database/');
-define('DIR_LANGUAGE', '/home/web/test.melle.online/www/admin/language/');
-define('DIR_TEMPLATE', '/home/web/test.melle.online/www/admin/view/template/');
-define('DIR_CONFIG', '/home/web/test.melle.online/www/system/config/');
-define('DIR_IMAGE', '/home/web/test.melle.online/www/image/');
-define('DIR_DOWNLOAD', '/home/web/test.melle.online/www/system/storage/download/');
-define('DIR_CACHE', '/home/web/test.melle.online/www/system/storage/cache/');
-define('DIR_LOGS', '/home/web/test.melle.online/www/system/storage/logs/');
+define('DIR_APPLICATION', '/home/web/work.melle.online/www/admin/');
+define('DIR_SYSTEM', '/home/web/work.melle.online/www/system/');
+define('DIR_DATABASE', '/home/web/work.melle.online/www/system/database/');
+define('DIR_LANGUAGE', '/home/web/work.melle.online/www/admin/language/');
+define('DIR_TEMPLATE', '/home/web/work.melle.online/www/admin/view/template/');
+define('DIR_CONFIG', '/home/web/work.melle.online/www/system/config/');
+define('DIR_IMAGE', '/home/web/work.melle.online/www/image/');
+define('DIR_DOWNLOAD', '/home/web/work.melle.online/www/system/storage/download/');
+define('DIR_CACHE', '/home/web/work.melle.online/www/system/storage/cache/');
+define('DIR_LOGS', '/home/web/work.melle.online/www/system/storage/logs/');
 
-define('DIR_MODIFICATION', '/home/web/test.melle.online/www/system/storage/modification/');
-define('DIR_UPLOAD', '/home/web/test.melle.online/www/system/storage/upload/');
+define('DIR_MODIFICATION', '/home/web/work.melle.online/www/system/storage/modification/');
+define('DIR_UPLOAD', '/home/web/work.melle.online/www/system/storage/upload/');
 define('DIR_STORAGE', DIR_SYSTEM . 'storage/');
-define('DIR_SESSION', '/home/web/test.melle.online/www/system/storage/session/');
+define('DIR_SESSION', '/home/web/work.melle.online/www/system/storage/session/');
 
-define('DIR_CATALOG', '/home/web/test.melle.online/www/catalog/');
+define('DIR_CATALOG', '/home/web/work.melle.online/www/catalog/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
