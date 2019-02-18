dc187b0c148c7ece82496efc0fb239d68e528b92
Ivan St <brokeyourbike@gmail.com>
2019-02-18 16:48:44 +0200

    ~

--- build.sh
+++ build.sh
@@ -21,6 +21,7 @@ yarn run prod-offersadmin
 yarn run prod-offers
 
 # CLEAR AFTER YARN
+rm -Rf ./.git/
 rm -Rf ./node_modules/
 rm -Rf ./catalog/view/javascript/melle/src/
 rm -Rf ./admin/view/javascript/import_1c/src/
@@ -70,7 +71,7 @@ sshpass -e ssh -o stricthostkeychecking=no $SSH_ADDRESS "bash -s
 "
 
 # CLONE TO PROD
-7z a -tzip foo.zip -x!build.sh
+7z a -tzip foo.zip -x\!build.sh
 sshpass -e scp -o stricthostkeychecking=no -r ./foo.zip $SSH_ADDRESS:$WORK_DIR
 sshpass -e ssh -o stricthostkeychecking=no $SSH_ADDRESS "7z -y x $WORK_DIR/foo.zip -o$WORK_DIR/"
 sshpass -e ssh -o stricthostkeychecking=no $SSH_ADDRESS "rm -f $WORK_DIR/foo.zip"
