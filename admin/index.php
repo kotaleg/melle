be90214f5abab59b3f970ad13949906eefd4ff75
Ivan St <brokeyourbike@gmail.com>
2019-03-01 20:05:01 +0200

    config fix

--- admin/index.php
+++ admin/index.php
@@ -3,15 +3,21 @@
 define('VERSION', '3.0.2.0');
 
 // Configuration
-if (is_file('config.php')) {
-    require_once('config.php');
+$config_file = 'config.php';
+if (isset($_SERVER['SERVER_ADDR'])
+&& strcmp($_SERVER['SERVER_ADDR'], '::1') === 0) {
+    $config_file = 'config.local.php';
+}
+
+if (is_file($config_file)) {
+    require_once($config_file);
 }
 
 // Install
-if (!defined('DIR_APPLICATION')) {
-    header('Location: ../install/index.php');
-    exit;
-}
+// if (!defined('DIR_APPLICATION')) {
+//     header('Location: ../install/index.php');
+//     exit;
+// }
 
 // Startup
 require_once(DIR_SYSTEM . 'startup.php');
--- index.php
+++ index.php
@@ -10,22 +10,25 @@ if (isset($_REQUEST['route'])
 }
 /* IMPORT HACK END */
 
-
-
-
 // Version
 define('VERSION', '3.0.2.0');
 
 // Configuration
-if (is_file('config.php')) {
-    require_once('config.php');
+$config_file = 'config.php';
+if (isset($_SERVER['SERVER_ADDR'])
+&& strcmp($_SERVER['SERVER_ADDR'], '::1') === 0) {
+    $config_file = 'config.local.php';
+}
+
+if (is_file($config_file)) {
+    require_once($config_file);
 }
 
 // Install
-if (!defined('DIR_APPLICATION')) {
-    header('Location: install/index.php');
-    exit;
-}
+// if (!defined('DIR_APPLICATION')) {
+//     header('Location: install/index.php');
+//     exit;
+// }
 
 // Startup
 require_once(DIR_SYSTEM . 'startup.php');
