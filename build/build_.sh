756c10acc89913c6229883da76e14e58c90fa0d6
Ivan St <brokeyourbike@gmail.com>
2020-01-12 03:47:00 +0200

    feat: check `yarn` status

--- build/build_.sh
+++ build/build_.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+
 clear
 shopt -s extglob
 
 SSH_ADDRESS=web@91.226.80.187
 
-GENERATED=$(date +%F--%N)
+# GENERATED=$(date +%F--%N)
 BACKUP_DIR=/home/web/melle.online/backup/$(date +%F)
 WORK_DIR=/home/web/melle.online/www
 WEBADDRESS=melle.online
@@ -16,28 +17,27 @@ mv ./config.work.php ./config.php
 mv ./admin/config.work.php ./admin/config.php
 
 echo "-- YARN RUN --"
-yarn install
-yarn run prod
-yarn run prod-import1c
-yarn run prod-offersadmin
-yarn run prod-offers
-yarn run prod-discount
-yarn run prod-sizelist
-yarn run prod-pricelist
-yarn run prod-melleb
+
+function is_yarn_fail()
+{
+    if [ "$(grep -c 'Done in' "$1")" -ne 1 ];then
+        rm -rf "$1"
+        echo "ERR: YARN SCRIPT FAIL"
+        exit 1
+    fi
+}
+
+yarn install > .yarn-result
+is_yarn_fail .yarn-result
+
+yarn prod > .yarn-result
+is_yarn_fail .yarn-result
 
 echo "-- CLEAR AFTER --"
 rm -Rf ./.git/
 rm -Rf ./node_modules/
 rm -Rf ./checkurl/
 rm -Rf ./catalog/view/javascript/melle/src/
-rm -Rf ./admin/view/javascript/import_1c/src/
-rm -Rf ./admin/view/javascript/super_offers/src/
-rm -Rf ./admin/view/javascript/super_offers_admin/src/
-rm -Rf ./admin/view/javascript/pro_discount/src/
-rm -Rf ./admin/view/javascript/size_list/src/
-rm -Rf ./admin/view/javascript/price_list/src/
-rm -Rf ./admin/view/javascript/melle_blocks/src/
 rm -Rf ./opt/
 rm -f README.md
 
@@ -48,7 +48,7 @@ sshpass -V
 export SSHPASS=$K8S_SECRET_SSH
 
 echo "-- BACKUP --"
-sshpass -e ssh -o stricthostkeychecking=no "$SSH_ADDRESS" "bash -s" < ./build/backup.sh $BACKUP_DIR $WORK_DIR
+sshpass -e ssh -o stricthostkeychecking=no "$SSH_ADDRESS" "bash -s" < ./build/backup.sh "$BACKUP_DIR" "$WORK_DIR"
 
 echo "-- COMPRESS FILES --"
 7z a -tzip foo.zip -x\!build -x\!.git
