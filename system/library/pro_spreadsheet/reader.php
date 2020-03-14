ae285436396f5ba648719989037401bf835dfc91
Ivan St <brokeyourbike@gmail.com>
2020-03-14 22:47:07 +0200

    feat: install `pro_spreadsheet` library

---
+++ system/library/pro_spreadsheet/reader.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ *  location: system/library
+ */
+
+namespace pro_spreadsheet;
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+use PhpOffice\PhpSpreadsheet\IOFactory;
+
+class reader extends IOFactory
+{
+    //
+}
