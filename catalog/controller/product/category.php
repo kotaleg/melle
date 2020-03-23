e55d32112142fb2c8438b8b2d11a44222ae7e67b
Ivan St <brokeyourbike@gmail.com>
2020-03-23 23:09:13 +0200

    feat: show description on category page

--- catalog/controller/product/category.php
+++ catalog/controller/product/category.php
@@ -128,7 +128,7 @@ class ControllerProductCategory extends Controller {
             //     $data['thumb'] = '';
             // }
 
-            // $data['description'] = html_entity_decode($category_info['description'], ENT_QUOTES, 'UTF-8');
+            $data['description'] = html_entity_decode($category_info['description'], ENT_QUOTES, 'UTF-8');
             // $data['compare'] = $this->url->link('product/compare');
 
             // $url = '';
