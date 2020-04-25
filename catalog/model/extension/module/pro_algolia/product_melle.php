0aa4aae04540346910757f32a78faf9999327d6c
Ivan St <brokeyourbike@gmail.com>
2020-04-25 20:56:32 +0300

    fix: add required values for the render to the product data for algolia

--- catalog/model/extension/module/pro_algolia/product_melle.php
+++ catalog/model/extension/module/pro_algolia/product_melle.php
@@ -36,8 +36,8 @@ class ModelExtensionModulePROAlgoliaProductMelle extends Model
         }
         $image = $this->model_tool_image->resize(
             $productData['image'], 
-            $this->config->get('theme_' . $this->config->get('config_theme') . '_image_thumb_width'), 
-            $this->config->get('theme_' . $this->config->get('config_theme') . '_image_thumb_height'), 
+            $this->config->get('theme_' . $this->config->get('config_theme') . '_image_product_width'), 
+            $this->config->get('theme_' . $this->config->get('config_theme') . '_image_product_height'), 
             true
         );
 
@@ -50,17 +50,71 @@ class ModelExtensionModulePROAlgoliaProductMelle extends Model
             ->getDescription($productId);
         /* EXTRA DESCRIPTION END */
 
+        $manufacturer = utf8_strtolower($productData['manufacturer']);
+
+        $manufacturerAltNames = array_map(function($row) {
+            return utf8_strtolower($row['altName']);
+        }, $this->getManufacturerAltNamesById($productData['manufacturer_id']));
+
+        $productAltNames = $this->generateProductAltNames(
+            $h1,
+            $manufacturer,
+            $manufacturerAltNames
+        );
+
+        $this->load->model('extension/module/super_offers');
+        $defaultvalues = $this->model_extension_module_super_offers
+            ->getDefaultValues($productData['product_id'], $productData);
+
+        $price = $defaultvalues['price'];
+        $special = $defaultvalues['special'];
+
+        /* SPECIAL TEXT START */
+        $this->load->model('extension/total/pro_discount');
+        $specialText = $this->model_extension_total_pro_discount
+            ->getSpecialText($productData['product_id'], true);
+        /* SPECIAL TEXT START */
+
         return array(
             'objectID' => \pro_algolia\id::generateIdForProduct((int) $productId),
             'productId' => (int) $productId,
+            
             'name' => $productData['name'],
             'h1' => $h1,
             'description' => html_entity_decode($productData['description'], ENT_QUOTES, 'UTF-8'),
             'smallDescription' => html_entity_decode($smallDescription, ENT_QUOTES, 'UTF-8'),
             'extraDescription' => html_entity_decode($extraDescription, ENT_QUOTES, 'UTF-8'),
-            'manufacturer' => $productData['manufacturer'],
+            'productAltNames' => $productAltNames,
+
+            'manufacturer' => $manufacturer,
+            'manufacturerAltNames' => $manufacturerAltNames,
+
             'image' => $image,
+
+            'price' => $price,
+            'special' => $special,
+            'specialText' => $specialText,
         );
     }
 
+    private function getManufacturerAltNamesById($manufacturerId)
+    {
+        return $this->db->query("SELECT * FROM `". DB_PREFIX ."manufacturer_alt_name`
+            WHERE `manufacturerId` = '" . (int) $manufacturerId . "'")->rows;
+    }
+
+    private function generateProductAltNames($productName, $manufacturerName, $manufacturerAltNames)
+    {
+        $productAltNames = array();
+
+        $productNameForReplace = utf8_strtolower($productName);
+
+        foreach ($manufacturerAltNames as $manufacturerAltName) {
+            if (strpos($productNameForReplace, $manufacturerName)) {
+                $productAltNames[] = str_replace($manufacturerName, $manufacturerAltName, $productNameForReplace);
+            }
+        }
+
+        return $productAltNames;
+    }
 }
