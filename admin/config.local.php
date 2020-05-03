0a1c40d3f73696616f8c6d91fa9e0ba8a92af0c1
Ivan St <brokeyourbike@gmail.com>
2020-05-03 20:56:33 +0300

    chore: local config

--- admin/config.local.php
+++ admin/config.local.php
@@ -1,42 +1,42 @@
 <?php
 // HTTP
-define('HTTP_SERVER', 'http://melle.test/admin/');
-define('HTTP_CATALOG', 'http://melle.test/');
+define('HTTP_SERVER', 'http://oc.test/302/melle-store/admin/');
+define('HTTP_CATALOG', 'http://oc.test/302/melle-store/');
 
 // HTTPS
-define('HTTPS_SERVER', 'http://melle.test/admin/');
-define('HTTPS_CATALOG', 'http://melle.test/');
+define('HTTPS_SERVER', 'http://oc.test/302/melle-store/admin/');
+define('HTTPS_CATALOG', 'http://oc.test/302/melle-store/');
 
 // HTTP OPT
 define('HTTP_OPT', HTTPS_CATALOG.'opt/');
 define('HTTP_OPT_ADMIN', HTTPS_CATALOG.'opt/admin/');
 
 // DIR
-define('DIR_OPT', '/Users/ivan/repos/melle/opt/');
-define('DIR_APPLICATION', '/Users/ivan/repos/melle/admin/');
-define('DIR_SYSTEM', '/Users/ivan/repos/melle/system/');
-define('DIR_DATABASE', '/Users/ivan/repos/melle/system/database/');
-define('DIR_LANGUAGE', '/Users/ivan/repos/melle/admin/language/');
-define('DIR_TEMPLATE', '/Users/ivan/repos/melle/admin/view/template/');
-define('DIR_CONFIG', '/Users/ivan/repos/melle/system/config/');
-define('DIR_IMAGE', '/Users/ivan/repos/melle/image/');
-define('DIR_DOWNLOAD', '/Users/ivan/repos/melle/system/storage/download/');
-define('DIR_CACHE', '/Users/ivan/repos/melle/system/storage/cache/');
-define('DIR_LOGS', '/Users/ivan/repos/melle/system/storage/logs/');
+define('DIR_OPT', '/Users/ivan/Documents/repo/oc/302/melle-store/opt/');
+define('DIR_APPLICATION', '/Users/ivan/Documents/repo/oc/302/melle-store/admin/');
+define('DIR_SYSTEM', '/Users/ivan/Documents/repo/oc/302/melle-store/system/');
+define('DIR_DATABASE', '/Users/ivan/Documents/repo/oc/302/melle-store/system/database/');
+define('DIR_LANGUAGE', '/Users/ivan/Documents/repo/oc/302/melle-store/admin/language/');
+define('DIR_TEMPLATE', '/Users/ivan/Documents/repo/oc/302/melle-store/admin/view/template/');
+define('DIR_CONFIG', '/Users/ivan/Documents/repo/oc/302/melle-store/system/config/');
+define('DIR_IMAGE', '/Users/ivan/Documents/repo/oc/302/melle-store/image/');
+define('DIR_DOWNLOAD', '/Users/ivan/Documents/repo/oc/302/melle-store/system/storage/download/');
+define('DIR_CACHE', '/Users/ivan/Documents/repo/oc/302/melle-store/system/storage/cache/');
+define('DIR_LOGS', '/Users/ivan/Documents/repo/oc/302/melle-store/system/storage/logs/');
 
-define('DIR_MODIFICATION', '/Users/ivan/repos/melle/system/storage/modification/');
-define('DIR_UPLOAD', '/Users/ivan/repos/melle/system/storage/upload/');
+define('DIR_MODIFICATION', '/Users/ivan/Documents/repo/oc/302/melle-store/system/storage/modification/');
+define('DIR_UPLOAD', '/Users/ivan/Documents/repo/oc/302/melle-store/system/storage/upload/');
 define('DIR_STORAGE', DIR_SYSTEM . 'storage/');
-define('DIR_SESSION', '/Users/ivan/repos/melle/system/storage/session/');
+define('DIR_SESSION', '/Users/ivan/Documents/repo/oc/302/melle-store/system/storage/session/');
 
-define('DIR_CATALOG', '/Users/ivan/repos/melle/catalog/');
+define('DIR_CATALOG', '/Users/ivan/Documents/repo/oc/302/melle-store/catalog/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
 define('DB_HOSTNAME', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', 'root');
-define('DB_DATABASE', 'melle-work');
+define('DB_DATABASE', 'melle-store');
 define('DB_PORT', '3306');
 define('DB_PREFIX', 'oc_');
 // OpenCart API
