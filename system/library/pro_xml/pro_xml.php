9904a57503edb68bb62763c0cb3fce85ce76aa1a
Ivan St <brokeyourbike@gmail.com>
2020-11-06 19:47:13 +0200

    feat: add pro_xml library

---
+++ system/library/pro_xml/pro_xml.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace pro_xml;
+require_once __DIR__ . '/vendor/autoload.php';
+
+use \FluidXml\FluidXml;
+use \FluidXml\FluidNamespace;
+use function \FluidXml\fluidxml;
+use function \FluidXml\fluidns;
+use function \FluidXml\fluidify;
+
+class pro_xml extends FluidXml
+{
+    //
+}
---
+++ system/library/pro_xml/vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'f6b40bc7686296146c2adbf6a8daeafb5e96a24e',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'f6b40bc7686296146c2adbf6a8daeafb5e96a24e',
+    ),
+    'servo/fluidxml' => 
+    array (
+      'pretty_version' => '1.21',
+      'version' => '1.21.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'b6fb8de6ff781479c9e4fb6e9f27b5af1e87dc4b',
+    ),
+  ),
+);
