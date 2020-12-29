568b90a7bf8c2457d6f42af3e4b3a05156fce015
Ivan Stasiuk <brokeyourbike@gmail.com>
2020-12-29 05:39:40 +0200

    fix: sort cart compare options

--- catalog/model/checkout/cart.php
+++ catalog/model/checkout/cart.php
@@ -50,6 +50,8 @@ class ModelCheckoutCart extends Model
                     $optionsForCompare[(string) $option['product_option_id']] = (string) $option['product_option_value_id'];
                 }
 
+                asort($optionsForCompare);
+
                 // Display prices
                 if ($this->customer->isLogged() || !$this->config->get('config_customer_price')) {
                     $unit_price = $this->tax->calculate($product['price'], $product['tax_class_id'], $this->config->get('config_tax'));
