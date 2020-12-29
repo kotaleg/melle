0c9ab2b998349a964f48d06f224b2d32e800dd77
Ivan Stasiuk <brokeyourbike@gmail.com>
2020-12-29 05:37:56 +0200

    chore: use @ import

--- catalog/view/theme/melle/javascript/src/router/index.js
+++ catalog/view/theme/melle/javascript/src/router/index.js
@@ -1,9 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
-import Catalog from './../components/catalog/Catalog.vue'
+import Catalog from '@/components/catalog/Catalog.vue'
 
 Vue.use(Router)
+
 export default new Router({
   mode: 'history',
   routes: [{ path: '*', component: Catalog, props: true }],
