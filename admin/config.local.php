5ff6e2a46e91d0e7432fd5b02460066ad0f85a8d
Ivan Stasiuk <brokeyourbike@gmail.com>
2021-01-04 11:10:14 +0200

    chore: move local to new folder

--- admin/config.local.php
+++ admin/config.local.php
@@ -12,24 +12,24 @@ define('HTTP_OPT', HTTPS_CATALOG.'opt/');
 define('HTTP_OPT_ADMIN', HTTPS_CATALOG.'opt/admin/');
 
 // DIR
-define('DIR_OPT', '/Users/ivan/Documents/repo/oc/302/melle-store/opt/');
-define('DIR_APPLICATION', '/Users/ivan/Documents/repo/oc/302/melle-store/admin/');
-define('DIR_SYSTEM', '/Users/ivan/Documents/repo/oc/302/melle-store/system/');
-define('DIR_DATABASE', '/Users/ivan/Documents/repo/oc/302/melle-store/system/database/');
-define('DIR_LANGUAGE', '/Users/ivan/Documents/repo/oc/302/melle-store/admin/language/');
-define('DIR_TEMPLATE', '/Users/ivan/Documents/repo/oc/302/melle-store/admin/view/template/');
-define('DIR_CONFIG', '/Users/ivan/Documents/repo/oc/302/melle-store/system/config/');
-define('DIR_IMAGE', '/Users/ivan/Documents/repo/oc/302/melle-store/image/');
-define('DIR_DOWNLOAD', '/Users/ivan/Documents/repo/oc/302/melle-store/system/storage/download/');
-define('DIR_CACHE', '/Users/ivan/Documents/repo/oc/302/melle-store/system/storage/cache/');
-define('DIR_LOGS', '/Users/ivan/Documents/repo/oc/302/melle-store/system/storage/logs/');
+define('DIR_OPT', '/Users/ivan/Documents/repos/oc/302/melle-store/opt/');
+define('DIR_APPLICATION', '/Users/ivan/Documents/repos/oc/302/melle-store/admin/');
+define('DIR_SYSTEM', '/Users/ivan/Documents/repos/oc/302/melle-store/system/');
+define('DIR_DATABASE', '/Users/ivan/Documents/repos/oc/302/melle-store/system/database/');
+define('DIR_LANGUAGE', '/Users/ivan/Documents/repos/oc/302/melle-store/admin/language/');
+define('DIR_TEMPLATE', '/Users/ivan/Documents/repos/oc/302/melle-store/admin/view/template/');
+define('DIR_CONFIG', '/Users/ivan/Documents/repos/oc/302/melle-store/system/config/');
+define('DIR_IMAGE', '/Users/ivan/Documents/repos/oc/302/melle-store/image/');
+define('DIR_DOWNLOAD', '/Users/ivan/Documents/repos/oc/302/melle-store/system/storage/download/');
+define('DIR_CACHE', '/Users/ivan/Documents/repos/oc/302/melle-store/system/storage/cache/');
+define('DIR_LOGS', '/Users/ivan/Documents/repos/oc/302/melle-store/system/storage/logs/');
 
-define('DIR_MODIFICATION', '/Users/ivan/Documents/repo/oc/302/melle-store/system/storage/modification/');
-define('DIR_UPLOAD', '/Users/ivan/Documents/repo/oc/302/melle-store/system/storage/upload/');
+define('DIR_MODIFICATION', '/Users/ivan/Documents/repos/oc/302/melle-store/system/storage/modification/');
+define('DIR_UPLOAD', '/Users/ivan/Documents/repos/oc/302/melle-store/system/storage/upload/');
 define('DIR_STORAGE', DIR_SYSTEM . 'storage/');
-define('DIR_SESSION', '/Users/ivan/Documents/repo/oc/302/melle-store/system/storage/session/');
+define('DIR_SESSION', '/Users/ivan/Documents/repos/oc/302/melle-store/system/storage/session/');
 
-define('DIR_CATALOG', '/Users/ivan/Documents/repo/oc/302/melle-store/catalog/');
+define('DIR_CATALOG', '/Users/ivan/Documents/repos/oc/302/melle-store/catalog/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
--- config.local.php
+++ config.local.php
@@ -9,22 +9,22 @@ define('HTTPS_SERVER', 'http://oc.test/302/melle-store/');
 // define('HTTP_OPT', HTTPS_SERVER.'opt/');
 
 // DIR
-// define('DIR_OPT', '/Users/ivan/Documents/repo/oc/302/melle-opt/');
-define('DIR_APPLICATION', '/Users/ivan/Documents/repo/oc/302/melle-store/catalog/');
-define('DIR_SYSTEM', '/Users/ivan/Documents/repo/oc/302/melle-store/system/');
-define('DIR_DATABASE', '/Users/ivan/Documents/repo/oc/302/melle-store/system/database/');
-define('DIR_LANGUAGE', '/Users/ivan/Documents/repo/oc/302/melle-store/catalog/language/');
-define('DIR_TEMPLATE', '/Users/ivan/Documents/repo/oc/302/melle-store/catalog/view/theme/');
-define('DIR_CONFIG', '/Users/ivan/Documents/repo/oc/302/melle-store/system/config/');
-define('DIR_IMAGE', '/Users/ivan/Documents/repo/oc/302/melle-store/image/');
-define('DIR_DOWNLOAD', '/Users/ivan/Documents/repo/oc/302/melle-store/system/storage/download/');
-define('DIR_CACHE', '/Users/ivan/Documents/repo/oc/302/melle-store/system/storage/cache/');
-define('DIR_LOGS', '/Users/ivan/Documents/repo/oc/302/melle-store/system/storage/logs/');
+// define('DIR_OPT', '/Users/ivan/Documents/repos/oc/302/melle-opt/');
+define('DIR_APPLICATION', '/Users/ivan/Documents/repos/oc/302/melle-store/catalog/');
+define('DIR_SYSTEM', '/Users/ivan/Documents/repos/oc/302/melle-store/system/');
+define('DIR_DATABASE', '/Users/ivan/Documents/repos/oc/302/melle-store/system/database/');
+define('DIR_LANGUAGE', '/Users/ivan/Documents/repos/oc/302/melle-store/catalog/language/');
+define('DIR_TEMPLATE', '/Users/ivan/Documents/repos/oc/302/melle-store/catalog/view/theme/');
+define('DIR_CONFIG', '/Users/ivan/Documents/repos/oc/302/melle-store/system/config/');
+define('DIR_IMAGE', '/Users/ivan/Documents/repos/oc/302/melle-store/image/');
+define('DIR_DOWNLOAD', '/Users/ivan/Documents/repos/oc/302/melle-store/system/storage/download/');
+define('DIR_CACHE', '/Users/ivan/Documents/repos/oc/302/melle-store/system/storage/cache/');
+define('DIR_LOGS', '/Users/ivan/Documents/repos/oc/302/melle-store/system/storage/logs/');
 
-define('DIR_MODIFICATION', '/Users/ivan/Documents/repo/oc/302/melle-store/system/storage/modification/');
-define('DIR_UPLOAD', '/Users/ivan/Documents/repo/oc/302/melle-store/system/storage/upload/');
+define('DIR_MODIFICATION', '/Users/ivan/Documents/repos/oc/302/melle-store/system/storage/modification/');
+define('DIR_UPLOAD', '/Users/ivan/Documents/repos/oc/302/melle-store/system/storage/upload/');
 define('DIR_STORAGE', DIR_SYSTEM . 'storage/');
-define('DIR_SESSION', '/Users/ivan/Documents/repo/oc/302/melle-store/system/storage/session/');
+define('DIR_SESSION', '/Users/ivan/Documents/repos/oc/302/melle-store/system/storage/session/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
