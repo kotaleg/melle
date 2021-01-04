85e4dafcaa4df35dde3d831eb0d8ac430224edff
Ivan Stasiuk <brokeyourbike@gmail.com>
2021-01-04 11:09:19 +0200

    chore: remove unused install script

--- admin/index.php
+++ admin/index.php
@@ -17,12 +17,6 @@ if (is_file($config_file)) {
     require_once($config_file);
 }
 
-// Install
-// if (!defined('DIR_APPLICATION')) {
-//     header('Location: ../install/index.php');
-//     exit;
-// }
-
 // Startup
 require_once(DIR_SYSTEM . 'startup.php');
 
