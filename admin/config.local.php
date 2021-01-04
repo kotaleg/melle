c3544de4dd3ce2e132f83ef1591cd4ac4835d7db
Ivan Stasiuk <brokeyourbike@gmail.com>
2021-01-04 11:15:05 +0200

    chore: use proper db host on local

--- admin/config.local.php
+++ admin/config.local.php
@@ -33,7 +33,7 @@ define('DIR_CATALOG', '/Users/ivan/Documents/repos/oc/302/melle-store/catalog/')
 
 // DB
 define('DB_DRIVER', 'mysqli');
-define('DB_HOSTNAME', 'localhost');
+define('DB_HOSTNAME', '127.0.0.1');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', 'root');
 define('DB_DATABASE', 'melle-store');
