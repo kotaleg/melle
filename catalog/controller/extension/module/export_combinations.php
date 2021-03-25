674a1a7264c54889e817e1c498cdcecd8527d3e9
Ivan Stasiuk <brokeyourbike@gmail.com>
2021-03-25 23:03:52 +0200

    feat: add export_combinations

---
+++ catalog/controller/extension/module/export_combinations.php
@@ -0,0 +1,43 @@
+<?php
+/*
+ *  location: catalog/controller
+ */
+class ControllerExtensionModuleExportCombinations extends Controller
+{
+    private $codename = 'export_combinations';
+    private $route = 'extension/module/export_combinations';
+    private $type = 'module';
+
+    private $setting = array();
+
+    public function __construct($registry)
+    {
+        parent::__construct($registry);
+
+        $this->load->language($this->route);
+
+        $this->load->model($this->route);
+        $this->load->model('extension/pro_patch/load');
+        $this->load->model('extension/pro_patch/language');
+        $this->load->model('extension/pro_patch/setting');
+
+        $this->setting = $this->model_extension_pro_patch_setting->getSetting($this->codename);
+        $this->extension_model = $this->{'model_'.str_replace("/", "_", $this->route)};
+    }
+
+    public function index()
+    {
+        $parsed = $this->model_extension_pro_patch_json->parseJson(file_get_contents('php://input'));
+
+        $json['codename'] = $this->codename;
+
+        if (isset($parsed['combinations']) && is_array($parsed['combinations'])) {
+            $json = $this->extension_model->fillCombinations();
+        } else {
+            $json['message'][] = 'Missing reuired data';
+        }
+
+        $this->response->addHeader('Content-Type: application/json');
+        $this->response->setOutput(json_encode($json));
+    }
+}
---
+++ catalog/model/extension/module/export_combinations.php
@@ -0,0 +1,50 @@
+<?php
+/*
+ *  location: catalog/model
+ */
+class ModelExtensionModuleExportCombinations extends Model
+{
+    private $codename = 'export_combinations';
+    private $route = 'extension/module/export_combinations';
+
+    private $setting = array();
+    private $super_offers;
+
+    public function __construct($registry)
+    {
+        parent::__construct($registry);
+
+        $this->load->model('extension/pro_patch/setting');
+        $this->load->model('extension/pro_patch/json');
+        $this->load->model('extension/pro_patch/db');
+        $this->load->model('extension/pro_patch/url');
+
+        $this->setting = $this->model_extension_pro_patch_setting->getSetting($this->codename);
+        $this->super_offers = new \super_offers($registry);
+    }
+
+    private function log($message)
+    {
+        if (isset($this->setting['debug']) && $this->setting['debug']) {
+            $this->log->write(strtoupper($this->codename)." :: {$message}");
+        }
+    }
+
+    public function fillCombinations($combinations)
+    {
+        $combinationsData = array();
+
+        foreach ($combinations as $combinationId) {
+            $combinationData = $this->super_offers->getCombination($combinationId);
+            if (!$combinationData || !isset($combinationData['image'])) {
+                $combinationsData[$combinationId] = false;
+                continue;
+            }
+            $combinationsData[$combinationId] = array(
+                'image' => $combinationData['image'],
+            );
+        }
+
+        return $combinationsData;
+    }
+}
